db8639c64f428fc007380583c5b4fb333ad8f400
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2021-04-21 13:59:07 +0300

    Shell Script for DB Modification

--- db_mod.sh
+++ db_mod.sh
@@ -4,17 +4,17 @@
 # db_mod_date finds last table modification date
 # db_mod_date_secs converts the db_mod_date to seconds
 # date_diff is the difference between today (now) and db_mod_date to find days between today and database last modification date
-# if a database is not modified or does not contain any table it will saved in the file not_modified
+# if a database is not modified or does not contain any table it will saved in the file unmodified_dbs
 
 # Creating Directory to Dump Databases
 mkdir -p -- /root/dbbackup
 
 # Dumping, Finding Creation Time, Last Modification Time and Cleaning Up Unwanted Databases
-if [ ! -e ./not_modified ]
+if [ ! -e ./unmodified_dbs ]
 then
-        touch not_modified
+        touch unmodified_dbs
 else
-        cat /dev/null > not_modified
+        cat /dev/null > unmodified_dbs
 fi
 
 file=./ignore_dbs
@@ -28,12 +28,12 @@ do
                 db_mod_table=$(mysql -e "use $DB;select table_name, max(update_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)" | grep -v "table_name")
                 if [ "$db_create_date" == NULL ]
                 then
-                        echo "$DB" >> ./not_modified        
+                        echo "$DB" >> ./unmodified_dbs        
                         echo "Database '$DB' does not contain any table."
                         echo ""
                 elif [ "$db_mod_date" == NULL ]
                 then            
-                        echo "$DB" >> ./not_modified        
+                        echo "$DB" >> ./unmodified_dbs        
                         echo "Database '$DB' last table was created on '$db_create_date' and was not modified after creation."
                         echo ""
 		else
