7cd2bcd2d044f49993c0f562488a491c4ca9c1fe
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2021-04-21 10:24:17 +0300

    Shell Script for DB Modification

--- db_mod.sh
+++ db_mod.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # db_create_date finds last table creation date
 # db_mod_date finds last table modification date
 # db_mod_date_secs converts the db_mod_date to seconds
@@ -8,20 +9,28 @@
 mkdir -p -- /root/dbbackup
 
 # Dumping, Finding Creation Time, Last Modification Time and Cleaning Up Unwanted Databases
-for DB in $(echo "show databases" | mysql | grep -Ev "^(Database|mysql|performance_schema|information_schema)$");
+file=./ignore_dbs
+while IFS= read -r line
 do
-	mysqldump $DB > /root/dbbackup/"$DB-$(date +%Y%m%d).sql";
-	db_create_date=$(mysql -e "use $DB;select max(create_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(create_time)" | awk "{print $1}")
-	db_mod_date=$(mysql -e "use $DB; select max(update_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)"| awk "{print $1}")
-	if [ $db_mod_date == NULL ]
-	then
-		echo "Database $DB's last table was created on $db_create_date and was not modified after creation."
-		echo ""
-	else
-		db_mod_date_secs=$(date --date="${db_mod_date}" +%s)
-		today=$(date +%s)
-		date_diff=$((($today - $db_mod_date_secs)/86400))
-		echo "Database $DB was last modified $date_diff days ago."
-		echo ""
-	fi
-done
+	for DB in $(echo "show databases" | mysql | grep -Ev "^($line)$");
+	do
+		mysqldump $DB > /root/dbbackup/"$DB-$(date +%Y%m%d).sql";
+		db_create_date=$(mysql -e "use $DB;select max(create_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(create_time)")
+		db_mod_date=$(mysql -e "use $DB;select max(update_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)")
+		if [ "$db_create_date" == NULL ]
+		then
+			echo "Database '$DB' does not contain any table."
+            		echo ""
+        	elif [ "$db_mod_date" == NULL ]
+        	then
+            		echo "Database '$DB' last table was created on '$db_create_date' and was not modified after creation."
+            		echo ""
+		else
+			db_mod_date_secs=$(date --date="${db_mod_date}" +%s)
+			today=$(date +%s)
+			date_diff=$((($today - $db_mod_date_secs)/86400))
+			echo "Database $DB was last modified $date_diff days ago."
+			echo ""
+		fi
+	done
+done < "$file"
