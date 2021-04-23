be8d52c6c790b1dcc664c5834aea074c67e688b1
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2021-04-23 09:47:57 +0300

    Shell Script for DB Modification

--- db_mod.sh
+++ db_mod.sh
@@ -12,40 +12,41 @@ mkdir -p -- /root/dbbackup
 # Dumping, Finding Creation Time, Last Modification Time and Cleaning Up Unwanted Databases
 if [ ! -e ./unmodified_dbs ]
 then
-	touch unmodified_dbs
+    touch unmodified_dbs
 else
-	cat /dev/null > unmodified_dbs
+    cat /dev/null > unmodified_dbs
+    echo "Databse - Table - Creation Time - Update Time" >> ./unmodified_dbs
 fi
 
 file=./ignore_dbs
 while IFS= read -r line
 do
-	for DB in $(echo "show databases" | mysql | grep -Ev "^($line)$");
-	do
-		mysqldump $DB > /root/dbbackup/"$DB-$(date +%Y%m%d).sql";
-		db_create_table=$(mysql -e "use $DB;select concat(table_name,', ',max(create_time)) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(create_time)" | grep -v "table_name")
-		db_create_date=$(mysql -e "use $DB;select max(create_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(create_time)")
-		db_mod_date=$(mysql -e "use $DB;select max(update_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)")
-		db_mod_table=$(mysql -e "use $DB;select concat(table_name,', ',max(create_time)) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)" | grep -v "table_name")
-		if [ "$db_create_date" == NULL ]
-		then
-			echo "$DB" >> ./unmodified_dbs
-	    		echo -e "Database '\033[1m$DB\033[0m' does not contain any table."
-			echo ""
-		elif [ "$db_mod_date" == NULL ]
-		then
-	    		echo "$DB" >> ./unmodified_dbs
-                        db_create_date_secs=$(date --date="${db_create_date}" +%s)
-			today=$(date "+%s")
-                        date_diff_create=$((($today - $db_create_date_secs)/86400))
-			echo -e "Database - '\033[1m$DB\033[0m', last created table and creation time - '\033[1m$db_create_table\033[0m', $date_diff_create days ago, and no modification occured."
-			echo ""
-		else
-	    		db_mod_date_secs=$(date --date="${db_mod_date}" +%s)
-			today=$(date "+%s")
-	    		date_diff_mod=$((($today - $db_mod_date_secs)/86400))
-			echo -e "Database - '\033[1m$DB\033[0m', last modified table and modification time - '\033[1m$db_mod_table\033[0m', $date_diff_mod days ago."
-			echo ""
-		fi
-    	done
+    for DB in $(echo "show databases" | mysql | grep -Ev "^($line)$");
+    do
+	mysqldump $DB > /root/dbbackup/"$DB-$(date +%Y%m%d).sql";
+        db_create_table=$(mysql -e "use $DB;select concat(table_name,' - ',max(create_time)) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(create_time)" | grep -v "table_name")
+        db_create_date=$(mysql -e "use $DB;select max(create_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(create_time)")
+     	db_mod_date=$(mysql -e "use $DB;select max(update_time) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)")
+       	db_mod_table=$(mysql -e "use $DB;select concat(table_name,' - ',max(update_time)) from information_schema.tables where table_schema = database();" | grep -v "\----" | grep -v "max(update_time)" | grep -v "table_name")
+        if [ "$db_create_date" == NULL ]
+        then
+            echo "$DB - - - - - - -" >> ./unmodified_dbs
+            echo -e "Database '\033[1m$DB\033[0m' does not contain any table."
+            echo "" 
+        elif [ "$db_mod_date" == NULL ]
+        then
+            echo "$DB - $db_create_table - $db_mod_date" >> ./unmodified_dbs
+            db_create_date_secs=$(date --date="${db_create_date}" +%s)
+	    today=$(date "+%s")
+            date_diff_create=$((($today - $db_create_date_secs)/86400))
+            echo -e "Database: '\033[1m$DB\033[0m', last created table and creation time: '\033[1m$db_create_table\033[0m', $date_diff_create days ago, and no modification occurred."
+            echo ""
+        else
+            db_mod_date_secs=$(date --date="${db_mod_date}" +%s)
+	    today=$(date "+%s")
+            date_diff_mod=$((($today - $db_mod_date_secs)/86400))
+            echo -e "Database: '\033[1m$DB\033[0m', last modified table and modification time: '\033[1m$db_mod_table\033[0m', $date_diff_mod days ago."
+            echo ""
+        fi
+    done
 done < "$file"
